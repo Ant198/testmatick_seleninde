a7406914b839b9fa11251007167f934a75dfac27
Anton <ridnovir@yahoo.com>
2025-02-13 12:49:31 +0200

    error test

--- src/test/java/AmazonBookSearchAndVerificationTest.java
+++ src/test/java/AmazonBookSearchAndVerificationTest.java
@@ -13,7 +13,7 @@ import storage.BooksList;
 public class AmazonBookSearchAndVerificationTest extends BaseTest {
     ElementsCollection books;
     BooksList booksList;
-    String basePageUrl = "https://www.amazon.com/";
+    String basePageUrl = "https://www.amazonn.com/";
     String bookPageUrl = "https://www.amazon.com/Head-First-Java-Brain-Friendly-Guide/dp/1491910771/ref=sr_1_3?dib=eyJ2IjoiMSJ9.VhdqJr59jl-7CJ3ERsIzDhwY0Yqmtq6zSQ3g-YBLqcuH4jbxNxHxI_oaEJ34po07SuLg5VeaNDQSHs0b7IdSteKVObRDXSpWsnrJnaOk_jnbqJ6cG5Iov4rmDUctlouQ6jc8Nlgu0fjfRedgr6fThsNLGcBZaVUtUtoxqTCXdprGIIOGGOEKqJ7Ftvu7ZR3qsZ-dObChiFblwTaFQh9hG65ByHgb-OnPk_hWafBE25E.u6ZcV2uYDzTAJIF7BT6z4WyyCgQjC4m8qR-hYxBO7jg&dib_tag=se&keywords=Java&qid=1728470947&s=books&sr=1-3 (дані про книгу теж потрібно витягти з її сторінки)";
 
     static {
