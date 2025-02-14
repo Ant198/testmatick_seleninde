7e770030281bd23920e8c1102089f9cda93f3353
Anton <ridnovir@yahoo.com>
2025-02-14 11:09:39 +0200

    working test 4

--- src/main/java/config/BaseTest.java
+++ src/main/java/config/BaseTest.java
@@ -1,15 +1,20 @@
 package config;
 
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeMethod;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
-/*
+
     @BeforeMethod
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        Configuration.browser = "chrome";
+        Configuration.headless = false;
+        Configuration.timeout = 30000;
         open("https://www.amazon.com/");
     }
 
- */
 }
--- src/test/java/testPages/AmazonBookSearchAndVerificationTest.java
+++ src/test/java/testPages/AmazonBookSearchAndVerificationTest.java
@@ -17,16 +17,15 @@ public class AmazonBookSearchAndVerificationTest extends BaseTest {
     BooksList booksList;
     String basePageUrl = "https://www.amazon.com/";
     String bookPageUrl = "https://www.amazon.com/Head-First-Java-Brain-Friendly-Guide/dp/1491910771/ref=sr_1_3?dib=eyJ2IjoiMSJ9.VhdqJr59jl-7CJ3ERsIzDhwY0Yqmtq6zSQ3g-YBLqcuH4jbxNxHxI_oaEJ34po07SuLg5VeaNDQSHs0b7IdSteKVObRDXSpWsnrJnaOk_jnbqJ6cG5Iov4rmDUctlouQ6jc8Nlgu0fjfRedgr6fThsNLGcBZaVUtUtoxqTCXdprGIIOGGOEKqJ7Ftvu7ZR3qsZ-dObChiFblwTaFQh9hG65ByHgb-OnPk_hWafBE25E.u6ZcV2uYDzTAJIF7BT6z4WyyCgQjC4m8qR-hYxBO7jg&dib_tag=se&keywords=Java&qid=1728470947&s=books&sr=1-3 (дані про книгу теж потрібно витягти з її сторінки)";
-
+/*
     static {
         Configuration.timeout = 30000; // 10 seconds
     }
-
+*/
     @Test
     public void test() {
         booksList = new BooksList();
-        AmazonSearchPage searchPage = open(basePageUrl, AmazonSearchPage.class);
-        // AmazonSearchPage searchPage = new AmazonSearchPage();
+        AmazonSearchPage searchPage = new AmazonSearchPage();
         searchPage.setPageUrl();
         searchPage.setPageTitle();
         searchPage.setFilter();
