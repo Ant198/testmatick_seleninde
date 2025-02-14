4d0bb9930347f8899256bf9df463e56568aa5fae
Anton <ridnovir@yahoo.com>
2025-02-14 13:03:59 +0200

    new test

--- src/main/java/config/BaseTest.java
+++ src/main/java/config/BaseTest.java
@@ -2,6 +2,7 @@ package config;
 
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -11,8 +12,9 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
+        //Configuration.browserCapabilities = new ChromeOptions().addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36");
         Configuration.browser = "chrome";
-        Configuration.headless = true;
+        Configuration.headless = false;
         Configuration.timeout = 30000;
         open("https://www.amazon.com/");
     }
